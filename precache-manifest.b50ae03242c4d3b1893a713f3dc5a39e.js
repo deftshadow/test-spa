a362a9e1da2b32e98c9bf99ef92bcb1a53115eb5
DeftShadow <deftshadow@gmail.com>
2020-09-25 12:13:26 +0300

    Updates

---
+++ precache-manifest.b50ae03242c4d3b1893a713f3dc5a39e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5235818388fa67e3459f6a8d884670",
+    "url": "/test-spa/index.html"
+  },
+  {
+    "revision": "89c7d9900ad44dd7a68f",
+    "url": "/test-spa/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "fa2d5ae31aae362c1f24",
+    "url": "/test-spa/static/css/main.b930db38.chunk.css"
+  },
+  {
+    "revision": "89c7d9900ad44dd7a68f",
+    "url": "/test-spa/static/js/2.b020665e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-spa/static/js/2.b020665e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa2d5ae31aae362c1f24",
+    "url": "/test-spa/static/js/main.2d295520.chunk.js"
+  },
+  {
+    "revision": "e3046ab76ffb50fdca18",
+    "url": "/test-spa/static/js/runtime-main.85c5bb95.js"
+  }
+]);
